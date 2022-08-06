75c7ab2fa9fb7649bf1e5eb51dd5821beaa31328
Nathan da Silva Lira <108292134+bynathan@users.noreply.github.com>
2022-08-06 11:30:34 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-window.confirm('Link do Diretorio do Projeto no GitHub: https://bynathan.github.io/projeto-figmaland-001/')
+window.confirm('Link do Diretorio do Projeto no GitHub: https://github.com/bynathan/projeto-figmaland-001 ')
+
+const menuHambButton = document.getElementsByClassName('menu-hamb');
+const navbarButton = document.getElementsByClassName('navbarbutton');
+const oneTwoTree = document.getElementsByClassName('onetwotree');
+const navbarMenu = document.getElementsByClassName('navbarmenu');
+
+const toggle_on = function() {
+    menuHambButton[0].classList.toggle('on');
+    navbarButton[0].classList.toggle('on');
+    oneTwoTree[0].classList.toggle('on');
+    navbarMenu[0].classList.toggle('on');
+};
+
+oneTwoTree[0].addEventListener('click', toggle_on);
